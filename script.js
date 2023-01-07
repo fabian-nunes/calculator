c4ab06099088e1caad441bd56d1d9d8c77c9056e
Fabian Nunes <fabiannunes12@gmail.com>
2023-01-07 21:28:17 +0000

    app almost done

--- script.js
+++ script.js
@@ -3,44 +3,88 @@
 $(document).ready(function() {
     var sign = false;
     var total = 0;
+    var calc = "";
+    var parcel = 0;
+    var cSign = "";
+    $("#valueInput").val("");
+
     $(".btnValue").click(function() {
         let value = $(this).val();
+        calc += value;
         let currentVal = $("#valueInput").val();
+        parcel = currentVal + value;
+
         $("#valueInput").val(currentVal + value);
         sign = false;
     });
+
     $("#clear").click(function() {
         $("#valueInput").val("");
     });
+
     $("#delete").click(function() {
         let currentVal = $("#valueInput").val();
         $("#valueInput").val(currentVal.slice(0, -1));
     });
+
     $(".btnSign").click(function() {
-        if (sign == false && $("#valueInput").val() != "") {
-            total = $("#valueInput").val();
-            $("#resultT").text(total);
-            sign = true;
+        if (sign == false) {
+            let value = $("#valueInput").val();
+            if (value != "") {
+                if (total == 0) {
+                    total = parseFloat(calc);
+                } else {
+                    calculate(cSign);
+                }
+                calc = "";
+                cSign = $(this).val();
+                $("#resultT").text(total);
+                sign = true;
+                $("#valueInput").val(total + cSign);
+            }
+        } else {
+            let value = $("#valueInput").val();
             let signT = $(this).val();
-            $("#valueInput").val(total + signT);
+            $("#valueInput").val(value.slice(0, -1) + signT);
         }
     });
+
     $("#equal").click(function() {
-        //split string by every sign
-        let currentVal = $("#valueInput").val();
-        let splitVal = currentVal.split(/(\+|\-|\*|\/)/);
-        for (let i = 0; i < splitVal.length; i++) {
-            if (splitVal[i] == "+") {
-                total = parseFloat(splitVal[i - 1]) + parseFloat(splitVal[i + 1]);
-            } else if (splitVal[i] == "-") {
-                total = parseFloat(splitVal[i - 1]) - parseFloat(splitVal[i + 1]);
-            } else if (splitVal[i] == "*") {
-                total = parseFloat(splitVal[i - 1]) * parseFloat(splitVal[i + 1]);
-            } else if (splitVal[i] == "/") {
-                total = parseFloat(splitVal[i - 1]) / parseFloat(splitVal[i + 1]);
-            }
-        }
+        calculate(cSign)
 
         $("#valueInput").val(total);
     });
+
+    function calculate(sign) {
+        let acc = "";
+        switch (sign) {
+            case "+":
+                total = total + parseInt(calc);
+                acc = total + "+" +calc + "=" + total;
+                break;
+            case "-":
+                total = total - parseInt(calc);
+                acc = total + "-" +calc + "=" + total;
+                break;
+            case "*":
+                total = total * parseInt(calc);
+                acc = total + "*" +calc + "=" + total;
+                break;
+            case "/":
+                total = total / parseInt(calc);
+                acc = total + "/" +calc + "=" + total;
+                break;
+        }
+        // Create a new <li> element
+        var li = document.createElement("li");
+        // Create a text node
+        var text = document.createTextNode(acc);
+        // Append the text to <li>
+        li.appendChild(text);
+        // Append <li> to <ul> with id="myList"
+        document.getElementById("last-calc").appendChild(li);
+        //Scroll to bottom
+        var objDiv = document.getElementById("last-calc");
+        objDiv.scrollTop = objDiv.scrollHeight;
+    }
 });
